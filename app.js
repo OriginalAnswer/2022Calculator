321ba9e5d475a6dc808223339d5f8d7938b3bcb3
OriginalAnswer <akiseonair@gmail.com>
2022-02-13 03:57:56 +0900

    up

--- app.js
+++ app.js
@@ -14,13 +14,30 @@ const equals = document.querySelector('equals');
 
 btns.map( b => {
     b.addEventListener('click', (e) => {
-        // console.log('clicked!');
-        // console.log(e);
-        // console.log(e.target);
-        // console.log(e.target.innerText);
+        // console.log('clicked!');console.log(e);console.log(e.target);console.log(e.target.innerText);
         switch(e.target.innerText){
+            case 'AC':
+                dp.innerText = '';
+                break;
+            case 'C':
+                if(dp.innerText){dp.innerText = dp.innerText.slice(0,-1);}
+                break;
+            case '=':
+                try{
+                    dp.innerText = eval(dp.innerText);
+                } catch {
+                    dp.innerText = '';
+                    alert('ERROR!');
+                }
+                
+                break;
             default:
-                dp.innerText += e.target.innerText;
+                if(dp.innerText === '0'){
+                    dp.innerText = '';
+                } else {
+                    dp.innerText += e.target.innerText;
+                }
+                
         }
     })
 })
