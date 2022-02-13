9a10a1ef7a4704492fad4e023201428e5aa2a16e
OriginalAnswer <akiseonair@gmail.com>
2022-02-14 03:26:22 +0900

    btn value
    html &# 문자 사용으로 인한 곱하기, 나누기 인식 오류 해결.
    hidden div 만들고, 각 버튼에 value 값 줘서 자바스크립트에서 e.target.value 값을 hidden div에 출력 계산은 hidden div에서 하지만 메인 디스플레이에 출력되는 것은 innerText로 출력되게 함.

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 const dp = document.getElementById('display');
+const dph = document.getElementById('displayhidden');
 
 const btns = Array.from(document.getElementsByTagName('button'));
 const nums = document.querySelectorAll("nums");
-const symb = document.querySelectorAll("symb");
-
-const clearall = document.querySelector('clearall');
-const plus = document.querySelector('plus');
-const mins = document.querySelector('mins');
-const divd = document.querySelector('divd');
-const times = document.querySelector('times');
-const equals = document.querySelector('equals');
-
 
 btns.map( b => {
     b.addEventListener('click', (e) => {
+        console.log(e.target.value);
         // console.log('clicked!');console.log(e);console.log(e.target);console.log(e.target.innerText);
-        switch(e.target.innerText){
+        switch(e.target.value){
             case 'AC':
-                dp.innerText = '';
+                dp.innerText = '0';
+                dph.innerText = '';
                 break;
             case 'C':
                 if(dp.innerText){dp.innerText = dp.innerText.slice(0,-1);}
+                if(dph.innerText){dph.innerText = dph.innerText.slice(0,-1);}
                 break;
             case '=':
                 try{
-                    dp.innerText = eval(dp.innerText);
+                    dp.innerText = eval(dph.innerText);
+                    dph.innerText = eval(dph.innerText);
                 } catch {
                     dp.innerText = '';
-                    alert('ERROR!');
+                    dph.innerText = '';
+                    alert('ERROR! 수식이 올바르지 않습니다.');
                 }
                 
                 break;
@@ -35,10 +32,19 @@ btns.map( b => {
                 if(dp.innerText === '0'){
                     dp.innerText = '';
                     dp.innerText += e.target.innerText;
+                    dph.innerText += e.target.value;
                 } else {
                     dp.innerText += e.target.innerText;
+                    dph.innerText += e.target.value;
                 }
                 
         }
     })
 })
+
+
+// 버튼의 벨류 값을 빈 배열에 푸쉬
+
+// 푸쉬한 배열에 .맵
+
+// 지우기 표시하기 결과내기
